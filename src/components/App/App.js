15ec6bd97fd63b97ad4d39c4a365a9be8b6890ee
malcodeman <malcodeman@users.noreply.github.com>
2018-02-12 23:24:53 +0100

    Experts section component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Video from '../Video/Video';
 import Form from '../Form/Form';
 import StorySection from '../StorySection/StorySection';
 import AppsSection from '../AppsSection/AppsSection';
+import ExpertsSection from '../ExpertsSection/ExpertsSection';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Form/>
       <StorySection/>
       <AppsSection/>
+      <ExpertsSection/>
     </React.Fragment>
   );
 }
--- src/components/AppsSection/AppsSection.js
+++ src/components/AppsSection/AppsSection.js
@@ -6,6 +6,10 @@ const AppsSection = () => {
         <section className="apps-section">
             <p className="p">IOS and Adroid <span className="dash"></span> apps with secret features to make 
                 Your training more fun</p>
+            <div className="vertical-line">
+                <span className="line"></span>
+                <span className="text">2018</span>
+            </div>            
         </section>
     )
 }
