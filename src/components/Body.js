e70477e7db86767a4a3262c5fcfd93110e5be813
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-11-22 23:45:06 +0100

    Adds body content

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import style from './body.module.css'
 
 function Body() {
-  return <main className={style.body}>body</main>
+  return (
+    <main className={style.body}>
+      <div className={style.content}>
+        <h1 className={style.tagline}>Brilliant. In every way.</h1>
+        <Link to="/shop" className={style.cta}>
+          Shop
+        </Link>
+      </div>
+    </main>
+  )
 }
 
 export default Body
---
+++ src/pages/shop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShopPage = () => (
+  <>
+    <div>shop</div>
+  </>
+)
+
+export default ShopPage
