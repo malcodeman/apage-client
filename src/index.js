d2cb71abfb5ec126ddbfee804c97f062ed800080
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-07-01 23:20:27 +0200

    Improves root file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./core/App";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { Router, Route } from "react-router-dom";
 
-import "./core/styles/index.css";
+import store from "./core/state/store";
+import theme from "./core/styles/themes/light";
+import history from "./core/routing/history";
+import Home from "./features/home/components/Home";
+import Login from "./features/auth/components/Login";
+import Signup from "./features/auth/components/Signup";
+import GlobalStyles from "./core/styles/GlobalStyles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const MOUNT_NODE = document.getElementById("root");
+
+function render() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <>
+          <Router history={history}>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Router>
+          <GlobalStyles />
+        </>
+      </ThemeProvider>
+    </Provider>,
+    MOUNT_NODE
+  );
+}
+
+render();
